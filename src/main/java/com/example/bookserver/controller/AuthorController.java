056d0626b5c22e63538d6a8a0d8c053ba08d53de
GorbachMaxim <gorbach203-sm@mail.ru>
2023-04-10 20:41:01 +0300

    Тестовый коммит

--- src/main/java/com/example/bookserver/controller/AuthorController.java
+++ src/main/java/com/example/bookserver/controller/AuthorController.java
@@ -1,4 +1,13 @@
 package com.example.bookserver.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/author")
+@RequiredArgsConstructor
+@CrossOrigin
 public class AuthorController {
 }
